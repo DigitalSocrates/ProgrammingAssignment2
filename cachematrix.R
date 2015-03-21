4a2d03a669345dadef9f0c4dc9325a74716e583a
DigitalSocrates <grigoriyshmigol@hotmail.com>
2015-03-20 22:21:51 -0700

    Update cachematrix.R
    fix for [Error in x$getmatrix : $ operator is invalid for atomic vectors]

--- cachematrix.R
+++ cachematrix.R
@@ -21,7 +21,7 @@ makeCacheMatrix <- function(x = matrix()) {
 ## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
 ## If the inverse has already been calculated (and the matrix has not changed), then the cachesolve 
 ## should retrieve the inverse from the cache.
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x=matrix(), ...) {
         matrix  <- x$getmatrix()
         if (!is.null(matrix)){
                 message("getting cached data")
